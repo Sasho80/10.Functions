2352fbe8752a81ab687615de3413174d5f9d3a63
Aleksandar <Sasho80@gbg.bg>
2023-12-24 01:40:02 +0200

    Add files via upload

---
+++ LearningFunctionsC++.cpp
@@ -0,0 +1,14 @@
+// First approach
+#include <iostream> 
+using namespace std;
+// Declaration
+void printSentence();
+int main() {
+	// Function call
+	printSentence();
+	return 0;
+}
+// Definition
+void printSentence() {
+	cout << "I am learning functions in C++." << endl;
+}
